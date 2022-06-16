e7e15c992b7d25caa66404f5a1a1201ede793e6b
Pawel Przybylski <pawel.przybylski1989@gmail.com>
2022-06-16 10:25:08 +0200

    Commit

--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -1,13 +1,17 @@
 public class Triangle {
 
-
-    public static boolean checkingTriangleIsRectangular(double a, double b, double c) {
-        if (Math.pow(c, 2) == Math.pow(a, 2) + Math.pow(b, 2)) {
-            return true;
+    public static boolean isRectangular(double a, double b, double c) {
+        if (a > b + c) {
+            return checkRectangularCondition(b, c, a);
+        } else if (b > a + c) {
+            return checkRectangularCondition(a, c, b);
         } else {
-            return false;
+            return checkRectangularCondition(a, b, c);
+        }
         }
 
+        private static boolean checkRectangularCondition ( double a, double b, double c){
+            return (Math.pow(c, 2) == Math.pow(a, 2) + Math.pow(b, 2));
     }
 
 }
--- src/test/java/TriangleTest.java
+++ src/test/java/TriangleTest.java
@@ -1,29 +1,53 @@
 import org.junit.Assert;
 import org.junit.Test;
-
 public class TriangleTest {
+
     @Test
-    public void shouldTriangleIsRectangular() {
+    public void ShouldBeNotRectangularWhenAHigherThanSumAAndB() {
+        //given
+        double a = 10;
+        double b = 4;
+        double c = 3;
+        //when
+        boolean triangleVerification = Triangle.isRectangular(a, b, c);
+        //then
+        Assert.assertFalse(triangleVerification);
+    }
+
+    @Test
+    public void ShouldBeNotRectangularWhenBHigherThanSumCAndB() {
+        //given
+        double a = 4;
+        double b = 10;
+        double c = 3;
+        //when
+        boolean triangleVerification = Triangle.isRectangular(a, b, c);
+        //then
+        Assert.assertFalse(triangleVerification);
+    }
+
+    @Test
+    public void shouldBeRectangular() {
         //given
         double a = 3;
         double b = 4;
         double c = 5;
         //when
-        boolean triangleVerification = Triangle.checkingTriangleIsRectangular(a, b, c);
+        boolean triangleVerification = Triangle.isRectangular(a, b, c);
         //then
-        Assert.assertEquals(true, triangleVerification);
+        Assert.assertTrue(triangleVerification);
     }
 
     @Test
-    public void shouldTriangleIsNonRectangular() {
+    public void shouldBeNotRectangular() {
         //given
         double a = 4;
         double b = 4;
         double c = 4;
         //when
-        boolean triangleVerification = Triangle.checkingTriangleIsRectangular(a, b, c);
+        boolean triangleVerification = Triangle.isRectangular(a, b, c);
         //then
-        Assert.assertEquals(false, triangleVerification);
+        Assert.assertFalse(triangleVerification);
     }
 
 }
